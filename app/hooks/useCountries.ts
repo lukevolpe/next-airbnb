8b9f025f1b626d0b3b250e5a865854816d40d3ec
Luke Volpe <luke.volpe@gmail.com>
2024-06-02 14:49:19 +0100

    Added map functionality

---
+++ app/hooks/useCountries.ts
@@ -0,0 +1,27 @@
+// Hook to pull countries through from the below package
+import countries from 'world-countries';
+
+// Formats the countries
+const formattedCountries = countries.map((country) => ({
+  value: country.cca2,
+  label: country.name.common,
+  flag: country.flag,
+  latlng: country.latlng,
+  region: country.region,
+}));
+
+// Creates the actual hook
+const useCountries = () => {
+  const getAll = () => formattedCountries;
+
+  const getByValue = (value: string) => {
+    return formattedCountries.find((item) => item.value === value);
+  };
+
+  return {
+    getAll,
+    getByValue,
+  };
+};
+
+export default useCountries;
