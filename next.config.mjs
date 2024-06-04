c407bebcf6e397009ba46ad5ad348b8da580dc0d
Luke Volpe <luke.volpe@gmail.com>
2024-06-04 18:07:37 +0100

    Completed listing creation journey via RentModal

--- next.config.mjs
+++ next.config.mjs
@@ -4,6 +4,7 @@ const nextConfig = {
     remotePatterns: [
       { hostname: 'avatars.githubusercontent.com' },
       { hostname: 'lh3.googleusercontent.com' },
+      { hostname: 'res.cloudinary.com' },
     ],
   },
 };
