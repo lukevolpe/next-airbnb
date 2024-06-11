b16077df42218e0ccf053b8b25bfe0501d7800ed
Luke Volpe <luke.volpe@gmail.com>
2024-06-11 19:16:26 +0100

    Completed reserve functionality

---
+++ app/actions/getReservations.ts
@@ -0,0 +1,55 @@
+import prisma from '@/app/libs/prismadb';
+
+interface IParams {
+  listingId?: string;
+  userId?: string;
+  authorId?: string;
+}
+
+export default async function getReservations(params: IParams) {
+  try {
+    const { listingId, userId, authorId } = params;
+
+    const query: any = {};
+
+    // If a listing id is sent, find all reservations for that listing
+    if (listingId) {
+      query.listingId = listingId;
+    }
+
+    // Search for all listings a certain user has
+    if (userId) {
+      query.userId = userId;
+    }
+
+    // Searches for all users that have a reservation across the same user's listings
+    if (authorId) {
+      query.listing = { userId: authorId };
+    }
+
+    const reservations = await prisma.reservation.findMany({
+      where: query,
+      include: {
+        listing: true,
+      },
+      orderBy: {
+        createdAt: 'desc',
+      },
+    });
+
+    const safeReservations = reservations.map((reservation) => ({
+      ...reservations,
+      createdAt: reservation.createdAt.toISOString(),
+      startDate: reservation.startDate.toISOString(),
+      endDate: reservation.endDate.toISOString(),
+      listing: {
+        ...reservation.listing,
+        createdAt: reservation.listing.createdAt.toISOString(),
+      },
+    }));
+
+    return safeReservations;
+  } catch (error: any) {
+    throw new Error(error);
+  }
+}
--- app/listings/[listingId]/page.tsx
+++ app/listings/[listingId]/page.tsx
@@ -2,13 +2,15 @@ import getCurrentUser from '@/app/actions/getCurrentUser';
 import getListingById from '@/app/actions/getListingById';
 import EmptyState from '@/app/components/EmptyState';
 import ListingClient from './ListingClient';
+import getReservations from '@/app/actions/getReservations';
 
 interface IParams {
-  listingID?: string;
+  listingId?: string;
 }
 
 const ListingPage = async ({ params }: { params: IParams }) => {
   const listing = await getListingById(params);
+  const reservations = await getReservations(params);
   const currentUser = await getCurrentUser();
 
   if (!listing) {
@@ -17,7 +19,11 @@ const ListingPage = async ({ params }: { params: IParams }) => {
 
   return (
     <div>
-      <ListingClient listing={listing} currentUser={currentUser} />
+      <ListingClient
+        listing={listing}
+        currentUser={currentUser}
+        reservations={reservations}
+      />
     </div>
   );
 };
