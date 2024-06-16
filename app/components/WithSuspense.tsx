8c661d36738e4925a012b8eea2a9b18f73437663
Luke Volpe <luke.volpe@gmail.com>
2024-06-16 17:16:27 +0100

    Added display name to suspense component

--- app/components/WithSuspense.tsx
+++ app/components/WithSuspense.tsx
@@ -1,14 +1,20 @@
 import React, { Suspense } from 'react';
 
-const WithSuspense = <P extends object>(
+const withSuspense = <P extends object>(
   Component: React.ComponentType<P>,
   fallback: React.ReactNode = <div>Loading...</div>
 ) => {
-  return (props: P) => (
+  const WrappedComponent = (props: P) => (
     <Suspense fallback={fallback}>
       <Component {...props} />
     </Suspense>
   );
+
+  WrappedComponent.displayName = `withSuspense(${
+    Component.displayName || Component.name || 'Component'
+  })`;
+
+  return WrappedComponent;
 };
 
-export default WithSuspense;
+export default withSuspense;
